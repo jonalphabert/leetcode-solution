fab481ae35d05b031d935774bcf04415a23122de
Jonathan Alphabert <jonathanalphabert0504@gmail.com>
2025-09-03 09:46:46 +0700

    Change Solution on 3025

--- group_3000-3199/3025. Find the Number of Ways to Place People I/solution.go
+++ group_3000-3199/3025. Find the Number of Ways to Place People I/solution.go
@@ -13,10 +13,10 @@ func numberOfPairs(points [][]int) int {
     n := len(points)
 
     sort.Slice(points, func(i, j int) bool {
-		if points[i][1] == points[j][1] { 
-			return points[i][0] < points[j][0] 
+		if points[i][0] == points[j][0] { 
+			return points[i][1] > points[j][1] 
 		}
-		return points[i][1] > points[j][1] 
+		return points[i][0] < points[j][0] 
 	})
 
     for i := 0 ; i < n ; i++ {
