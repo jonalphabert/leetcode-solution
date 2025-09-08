f5cc00eac86f6fe636e0ea96a59010c2d377b5cd
Jonathan Alphabert <jonathanalphabert0504@gmail.com>
2025-09-08 14:40:51 +0700

    Added Solution for 0118. Pascal's Triangle

---
+++ group_0000-0199/0118. Pascal's Triangle/solution.go
@@ -0,0 +1,15 @@
+func generate(numRows int) [][]int {
+    res := make([][]int, numRows)
+
+    for i := 0 ; i < numRows; i++ {
+        for j:= 0 ; j < i+1; j++ {
+            if j==0 || j == i {
+                res[i] = append(res[i], 1)
+            } else {
+                res[i] = append(res[i], res[i-1][j-1]+res[i-1][j])
+            }
+        }
+    }
+
+    return res
+}
